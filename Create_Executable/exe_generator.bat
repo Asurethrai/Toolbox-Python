48e4f6e782c2fcc9e3f996c3596f6c47a75a31b5
Asurethrai <36038751+Asurethrai@users.noreply.github.com>
2018-10-29 17:12:54 +0100

    Revert "Update Create_Executable/exe_generator.bat"
    This reverts commit 76a037692e15578dd2e9bc8326f507bc62285418.

--- Create_Executable/exe_generator.bat
+++
@@ -1,6 +0,0 @@
-call "C:\Program Files (x86)\Python37-32\Scripts\pyinstaller" --icon=MFH_FT_Monitor.ico --clean -n MFH_FT_Monitor -w .\main.py  
-
-@echo copy  UI file in dist file
-copy .\*.ui dist\MFH_FT_Monitor\
-
-pause
